36090b343a13e6077d154b37e751e40144bf9aad
Chanaka <chanakawi@mitesp.com>
2022-06-03 16:36:32 +0530

    redux toolkit redux tool kit

---
+++ react-rtk-demo/src/features/user/userSlice.js
@@ -0,0 +1,54 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+const initialState = {
+  loading: false,
+  users: [],
+  error: '',
+}
+
+// Generated pending, fulfilled and rejected action types
+export const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
+  return axios
+    .get('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.data)
+  // .catch((error) => {
+  //   //error.message is the error message
+  //   dispatch(fetchUsersSuccess(error.message))
+  // })
+})
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfIceCreams--
+    },
+    restocked: (state, action) => {
+      state.numOfIceCreams += action.payload
+    },
+  },
+  // extraReducers: {
+  //   ['cake/ordered']: (state) => {
+  //     state.numOfIceCreams--
+  //   },
+  // },
+  extraReducers: (builder) => {
+    builder.addCase(fetchUsers.pending, (state) => {
+      state.loading = true
+    })
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      state.loading = false
+      state.users = action.payload
+      state.error = ''
+    })
+    builder.addCase(fetchUsers.rejected, (state, action) => {
+      state.loading = false
+      state.users = []
+      state.error = action.error.message
+    })
+  },
+})
+
+export default userSlice.reducer
